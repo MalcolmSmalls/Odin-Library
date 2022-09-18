e87298d01b500b4bf80d8717c1871ac0795ff404
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-18 10:58:05 -0700

    Create eventListener using javaScript

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 let library = [];
 
+const submitButton = document.querySelector('button')
+const formTitle = document.getElementById('bookTitle')
+const formAuthor = document.getElementById('author')
+const formPages = document.getElementById('pages')
+const formRead = document.getElementById('read')
+const formUnread = document.getElementById('unread')
+
+function addBook() {
+    
+}
+
+
+submitButton.addEventListener('click', addBook)
+
+
+
+
+
 function Book() {
 
 };
